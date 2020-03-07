e65130a042e47b2932f4e1f609c78bc20fe6b257
Ravi Ranjan <ravirnjn88@gmail.com>
2020-03-08 00:32:34 +0530

    admin file for products

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -3,9 +3,13 @@ from apps.products.models.product import Product
 from apps.products.models.images import Image
 from apps.products.models.option import Option
 from apps.products.models.option_value import OptionValue
+from apps.products.models.tags import Tag
+from apps.products.models.variant import Variant
 from apps.products.forms.images_form import ImageForm
 from apps.products.forms.option_form import OptionForm
 from apps.products.forms.option_value_form import OptionValueForm
+from apps.products.forms.tag_form import TagForm
+from apps.products.forms.variant_form import VariantForm
 import nested_admin
 
 
@@ -46,9 +50,31 @@ class OptionInline(nested_admin.NestedTabularInline):
     extra = 1
 
 
+class TagInline(nested_admin.NestedTabularInline):
+    """this inline class is used in Products for showing tags."""
+
+    model = Tag
+    form = TagForm
+    fields = ('name',)
+    # readonly_fields = ( )
+    show_add_link = True
+    # inlines = []
+    extra = 1
 
 
+class VariantInline(nested_admin.NestedStackedInline):
+    """this inline class is used in Products for showing Variant."""
 
+    model = Variant
+    form = VariantForm
+    fields = ('title', 'price', 'sku', 'position', 'compare_at_price',
+        	  'option1', 'option2', 'option3', 'taxable', 'barcode',
+        	  'weight', 'weight_unit', 'image', 'inventory_quantity',
+        	  'requires_shipping')
+    # readonly_fields = ( )
+    show_add_link = True
+    # inlines = []
+    extra = 1
 
 
 
@@ -64,5 +90,5 @@ class ProductAdmin(nested_admin.NestedModelAdmin):
     """Registering PRoduct in admin."""
 
     model = Product
-    inlines = [ImagesInline, OptionInline]
+    inlines = [ImagesInline, OptionInline, TagInline, VariantInline]
 
--- apps/products/models/variant.py
+++ apps/products/models/variant.py
@@ -20,23 +20,23 @@ class Variant(BaseModel):
     compare_at_price = models.DecimalField(max_digits=8, decimal_places=2, null=True)
     fulfillment_service = models.CharField(max_length=40, default='manual')
     inventory_management = models.CharField(max_length=40, null=True, default='blank')
-    option1 = models.ForeignKey(OptionValue, related_name='variant', on_delete=models.SET_NULL, null=True)
-    option2 = models.ForeignKey(OptionValue, related_name='variant', on_delete=models.SET_NULL, null=True)
-    option3 = models.ForeignKey(OptionValue, related_name='variant', on_delete=models.SET_NULL, null=True)
+    option1 = models.ForeignKey(OptionValue, related_name='variant1', on_delete=models.SET_NULL, null=True)
+    option2 = models.ForeignKey(OptionValue, related_name='variant2', on_delete=models.SET_NULL, null=True)
+    option3 = models.ForeignKey(OptionValue, related_name='variant3', on_delete=models.SET_NULL, null=True)
     taxable = models.BooleanField(default=True)
     barcode = models.CharField(max_length=200, null=True)
     weight = models.IntegerField(null=True)
     weight_unit = models.CharField(max_length=20, null=True, choices=WEIGHT)
-    image = models.ForeignKey(Image, related_name='variant', on_delete=models.SET_NULL)
+    image = models.ForeignKey(Image, related_name='variant', on_delete=models.SET_NULL, null=True)
     inventory_quantity = models.IntegerField()
     requires_shipping = models.BooleanField(default=True)
 
 
 
     class Meta:
-        db_table = "image"
-        verbose_name = "Image"
-        verbose_name_plural = "Images"
+        db_table = "variant"
+        verbose_name = "Variant"
+        verbose_name_plural = "Variants"
 
     def __unicode__(self):
         """Return name of entity."""
