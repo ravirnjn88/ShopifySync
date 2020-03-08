9b0ba07410c05e7fcb4a62bc85a47f727c0c44ed
Ravi Ranjan <ravirnjn88@gmail.com>
2020-03-08 14:31:16 +0530

    address and customer models

---
+++ apps/orders/models/address.py
@@ -0,0 +1,36 @@
+from django.db import models
+from shopifysync.models import BaseModel
+from .customer import Customer
+
+class Address(BaseModel):
+
+    shopify_address_id = models.BigIntegerField(unique=True)
+    first_name = models.CharField(max_length=64, null=True, blank=True)
+    last_name = models.CharField(max_length=64, null=True, blank=True)
+    company = models.CharField(max_length=64, null=True, blank=True)
+    address1 = models.CharField(max_length=256, blank=True)
+    address2 = models.CharField(max_length=256, blank=True)
+    city = models.CharField(max_length=64, blank=True)
+    province = models.CharField(max_length=128, null=True, blank=True)
+    country = models.CharField(max_length=64, blank=True)
+    zip = models.CharField(max_length=16, null=True, blank=True)
+    phone = models.CharField(max_length=32, null=True, blank=True)
+    province_code = models.CharField(max_length=32, null=True, blank=True)
+    country_code = models.CharField(max_length=32, blank=True, null=True)
+    country_name = models.CharField(max_length=32, blank=True, null=True)
+    default = models.BooleanField(default=False)
+    customer = models.ForeignKey(Customer, related_name='address',
+                                 on_delete=models.CASCADE)
+
+
+    class Meta:
+        db_table = "address"
+        verbose_name = "Address"
+        verbose_name_plural = "Addresses"
+
+    def __unicode__(self):
+        """Return name of entity."""
+        return "{}{}".format(self.first_name, self.address1)
+
+    def __str__(self):
+        return "{}{}".format(self.first_name, self.address1)
---
+++ apps/orders/models/customer.py
@@ -0,0 +1,37 @@
+from django.db import models
+from shopifysync.models import BaseModel
+
+class Customer(BaseModel):
+
+    shopify_cusomer_id = models.BigIntegerField(unique=True)
+    email = models.EmailField(null=True)
+    accepts_marketing = models.BooleanField(default=False)
+    first_name = models.CharField(max_length=104, blank=True)
+    last_name = models.CharField(max_length=104, blank=True)
+    orders_count = models.IntegerField()
+    state = models.CharField(max_length=64)
+    total_spent = models.DecimalField(max_digits=8, decimal_places=2)
+    last_order_id = models.IntegerField(null=True)
+    note = models.TextField(null=True, blank=True)
+    verified_email = models.BooleanField(default=False)
+    multipass_identified = models.CharField(max_length=64, null=True)
+    tax_exempt = models.BooleanField(default=False)
+    phone = models.CharField(max_length=20, null=True)
+    tags = models.TextField(null=True)
+    last_order_name = models.CharField(max_length=64,null=True)
+    currency = models.CharField(max_length=24)
+    marketing_opt_in_level = models.CharField(max_length=24, null=True)
+    # default_address = models.ForeignKey(Address, null = True, related_name = 'default_address')
+
+
+    class Meta:
+        db_table = "customer"
+        verbose_name = "Customer"
+        verbose_name_plural = "Customers"
+
+    def __unicode__(self):
+        """Return name of entity."""
+        return self.first_name
+
+    def __str__(self):
+        return self.first_name
